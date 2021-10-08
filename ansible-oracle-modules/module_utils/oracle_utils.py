b4a803c4d0a850b4d896abccb98e2383b54ad180
root <root@rhel7-a.prod.vmware.haf>
2021-10-08 18:53:53 +0200

    Use connect internal as sys

--- ansible-oracle-modules/module_utils/oracle_utils.py
+++ ansible-oracle-modules/module_utils/oracle_utils.py
@@ -7,21 +7,37 @@ def oracle_connect(module):
     Return: connection
     """
 
+    oracle_home = module.params["oracle_home"]
+    hostname = module.params["hostname"]
+    port = module.params["port"]
+    service_name = module.params["service_name"]
+    sid = module.params["sid"]
+    user = module.params["user"].lower()
+    password = module.params["password"]
+    mode = module.params["mode"].lower()
+
     if oracle_home is not None:
         os.environ['ORACLE_HOME'] = oracle_home.rstrip('/')
     elif 'ORACLE_HOME' in os.environ:
         oracle_home = os.environ['ORACLE_HOME']
 
+    if sid:
+        os.environ['ORACLE_SID'] = sid
+    elif 'ORACLE_SID' in os.environ:
+        sid = os.environ['ORACLE_SID']
+        
     wallet_connect = '/@%s' % service_name
+    sysdba_connect = '/' % service_name
     try:
-        if (not user and not password ): # If neither user or password is supplied, the use of an oracle wallet is assumed
+        if not user and not password: # If neither user or password is supplied, the use of an oracle wallet is assumed
             if mode == 'sysdba':
-                connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect, mode=cx_Oracle.SYSDBA)
             else:
-                connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect)
 
+        else user == 'sys' and mode == 'sysdba' and not password:
+            conn = cx_Oracle.connect(sysdba_connect, mode=cx_Oracle.SYSDBA)
+            
         elif (user and password ):
             if mode == 'sysdba':
                 dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
@@ -41,4 +57,3 @@ def oracle_connect(module):
         module.fail_json(msg=msg, changed=False)
 
     return conn
-
