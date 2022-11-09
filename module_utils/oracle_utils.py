a21c9d03830298cc042e0be95c8d61fde40499ac
Ivan Brezina <ibrezina@ibrezina.net>
2022-11-09 23:51:24 +0100

    ???

--- module_utils/oracle_utils.py
+++ module_utils/oracle_utils.py
@@ -1,53 +1,50 @@
 
 from ansible.module_utils.basic import *
 
-try:
-	import cx_Oracle
-except ImportError:
-	cx_oracle_exists = False
-else:
-	cx_oracle_exists = True
-
 def oracle_connect(module):
     """
     Connect to the database using parameter provided by Ansible module instance.
     Return: connection
     """
 
-    oracle_home = module.params["oracle_home"]
+    try:
+        import cx_Oracle
+    except ImportError:
+        cx_oracle_exists = False
+    else:
+        cx_oracle_exists = True
+
+    if not cx_oracle_exists:
+        module.fail_json(msg="The cx_Oracle module is required. 'pip install cx_Oracle' should do the trick. If cx_Oracle is installed, make sure ORACLE_HOME & LD_LIBRARY_PATH is set")
+        
+    if "oracle_home" in module.params:
+        oracle_home = module.params["oracle_home"]
+    else:
+        oracle_home = None
     hostname = module.params["hostname"]
     port = module.params["port"]
     service_name = module.params["service_name"]
-    sid = module.params["sid"]
-    user = module.params["user"].lower()
+    user = module.params["user"]
     password = module.params["password"]
-    mode = module.params["mode"].lower()
+    mode = module.params["mode"]
 
     if oracle_home is not None:
         os.environ['ORACLE_HOME'] = oracle_home.rstrip('/')
     elif 'ORACLE_HOME' in os.environ:
         oracle_home = os.environ['ORACLE_HOME']
 
-    if sid:
-        os.environ['ORACLE_SID'] = sid
-    elif 'ORACLE_SID' in os.environ:
-        sid = os.environ['ORACLE_SID']
-        
     wallet_connect = '/@%s' % service_name
+
     sysdba_connect = '/'
     try:
-        if not user and not password: # If neither user or password is supplied, the use of an oracle wallet is assumed
+        if (not user and not password ): # If neither user or password is supplied, the use of an oracle wallet is assumed
             if mode == 'sysdba':
-                connect = wallet_connect
-                conn = cx_Oracle.connect(wallet_connect, mode=cx_Oracle.SYSDBA)
+                connect = sysdba_connect
+                conn = cx_Oracle.connect(sysdba_connect, mode=cx_Oracle.SYSDBA)
             else:
                 connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect)
 
-        elif user == 'sys' and mode == 'sysdba' and not password:
-            connect = sysdba_connect
-            conn = cx_Oracle.connect(sysdba_connect, mode=cx_Oracle.SYSDBA)
-            
         elif (user and password ):
             if mode == 'sysdba':
                 dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
@@ -67,3 +64,4 @@ def oracle_connect(module):
         module.fail_json(msg=msg, changed=False)
 
     return conn
+
